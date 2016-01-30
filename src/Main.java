8314a5a1125999314db89578f4e9310396b2ea37
bendmahon <bendmahon@gmail.com>
2016-01-30 10:27:23 -0600

    Key Input Added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.awt.Canvas;
+
 /**
  * Created by hibbscm on 1/30/2016.
  */
-public class Main {
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = (int) 800;
+    public static final int HEIGHT = (int) 600;
+
+    public final String TITLE = "Game Title Here";
+
+    private boolean running = false;
+    private Thread thread;
+
+    private synchronized void start() {
+    }
 }
