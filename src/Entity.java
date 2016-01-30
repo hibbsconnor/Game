9c2edeb6f6be062cbd01b3499e79eec40240a2cc
hibbscm <hibbsconnor@yahoo.com>
2016-01-30 11:57:24 -0600

    Worked more with Entity and Player BufferedImages are now a property of each individual player Added in key input to use with movement

--- src/Entity.java
+++ src/Entity.java
@@ -3,38 +3,29 @@ import java.awt.image.BufferedImage;
 
 public abstract class Entity {
 
-    private boolean isSolid = true; //entity solid by default
-    private BufferedImage sprite;
-    private BufferedImage[] spriteArray;
-    private Point position;
-    private Point velocity;
+    protected boolean isSolid = true; //entity solid by default
+    protected Point position;
+    protected Point velocity;
+    protected Rectangle bounds; //collision bounds
 
-    public Entity(BufferedImage sprite, Point position, Point velocity){
-        this.sprite = sprite;
+    public Entity(Point position, Point velocity){
         this.position = position;
         this.velocity =  velocity;
     }
 
-    public Entity(BufferedImage[] spriteArray, Point position, Point velocity){
-        this.spriteArray = spriteArray;
-        this.position = position;
-        this.velocity =  velocity;
-    }
-
-    public Entity(BufferedImage sprite, float xPosition, float yPosition, float xVelocity, float yVelocity){
-        this.sprite = sprite;
+    public Entity(float xPosition, float yPosition, float xVelocity, float yVelocity){
         this.position.x = (int)xPosition;
         this.position.y = (int)yPosition;
         this.velocity.x = (int)xVelocity;
         this.velocity.y = (int)yVelocity;
     }
 
-    public Entity(BufferedImage[] spriteArray, float xPosition, float yPosition, float xVelocity, float yVelocity){
-        this.spriteArray = spriteArray;
-        this.position.x = (int)xPosition;
-        this.position.y = (int)yPosition;
-        this.velocity.x = (int)xVelocity;
-        this.velocity.y = (int)yVelocity;
+    public void tick(){
+
+    }
+
+    public void render(Graphics g){
+
     }
 
     public Point getPosition() {
@@ -51,13 +42,5 @@ public abstract class Entity {
 
     public void setVelocity(Point velocity) {
         this.velocity = velocity;
-    }
-
-    public BufferedImage getSprite() {
-        return sprite;
-    }
-
-    public BufferedImage[] getSpriteArray() {
-        return spriteArray;
     }
 }
