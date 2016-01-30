eb5da1ea25db9fafe64c48e00b8a5276b7403bcd
bendmahon <bendmahon@gmail.com>
2016-01-30 10:44:33 -0600

    Added Collisions

---
+++ src/Collisions.java
@@ -0,0 +1,15 @@
+import java.awt.Point;
+import java.awt.Rectangle;
+
+/**
+ * Created by mahonbd
+ */
+public class Collisions {
+    public static boolean rectCollision(Rectangle r1, Rectangle r2) {
+        return !(r2.x > r1.x + r1.width || r2.y > r1.y + r1.height || r2.x + r2.width < r1.x || r2.y + r2.height < r1.y);
+    }
+
+    public static boolean pointRectCollision(Point p, Rectangle rect){
+        return p.x>=rect.x&&p.x<=rect.x+rect.width&&p.y>=rect.y&&p.y<=rect.y+rect.height;
+    }
+}
