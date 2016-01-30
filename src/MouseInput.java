46c840b5160e70f3152f19ab667baf3cc09f8ed4
bendmahon <bendmahon@gmail.com>
2016-01-30 10:07:17 -0600

    Main class created

---
+++ src/MouseInput.java
@@ -0,0 +1,28 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseInput implements MouseListener{
+
+    public MouseInput(){
+
+    }
+
+    public void mouseEntered(MouseEvent e){
+        //System.out.println("In Box");
+    }
+
+    public void mouseExited(MouseEvent e ){
+        //System.out.println("out box");
+    }
+
+    public void mousePressed(MouseEvent e){
+        //System.out.println("press");
+    }
+
+    public void mouseReleased(MouseEvent e){
+        //System.out.println("release");
+    }
+
+    public void mouseClicked(MouseEvent e){
+    }
+}
